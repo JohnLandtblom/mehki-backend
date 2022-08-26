05c3bab5a7d0fdaf11d336b9364eed013688e929
Rimon Hanna <hannarimon1@gmail.com>
2022-08-26 16:36:32 +0200

    first working protoype of login module done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const userInfoHandler = (userInfo) => {
+    // Available in App.js
+    console.log(userInfo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Prop som hämtar userInfo till App.js */}
+      <LoginForm sendToApp={userInfoHandler} />
     </div>
   );
 }
