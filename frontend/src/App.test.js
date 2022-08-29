f686fde3a2bbd220f1f1828e2c1e3a64a92737e1
Jergie Mascan <jiji.mascan@gmail.com>
2022-08-29 12:15:19 +0200

    unit testing in form executed

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import LoginForm from "./components/LoginForm";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("should show password", () => {
+  render(<LoginForm />);
+  const input = screen.getByPlaceholderText("Password");
+  expect(input).toBeInTheDocument(input);
 });
