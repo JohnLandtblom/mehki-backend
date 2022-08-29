83801c922522c7c939a3871beb0f9c1b40e24f4d
Jergie Mascan <jiji.mascan@gmail.com>
2022-08-29 13:59:04 +0200

    simple backend unit test added

---
+++ backend/__tests__/server-test.js
@@ -0,0 +1,9 @@
+const serverTest = require("../server");
+const mongoose = require("mongoose");
+
+it("connects backend to Database", async () => {
+  const db = await mongoose.connect(
+    "mongodb+srv://db:swed123@cluster0.kbbyqnx.mongodb.net/?retryWrites=true&w=majority"
+  );
+  expect(db).toBe(db);
+});
