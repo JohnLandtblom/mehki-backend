30e1cd440a20d14fa1411b2c761a36893aa15406
Jiji <89918853+jergiemascan@users.noreply.github.com>
2022-08-30 10:33:00 +0200

    Delete frontend directory

--- frontend/src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import LoginForm from "../components/LoginForm";
-
-test("should show password", () => {
-  render(<LoginForm />);
-  const input = screen.getByPlaceholderText("Password");
-  expect(input).toBeInTheDocument(input);
-});
