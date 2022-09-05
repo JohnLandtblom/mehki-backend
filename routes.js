e68eb756107a22ef284812258a55b52ef9dbea5a
Cristian Berglund <Cristian.berglund@hotmail.se>
2022-09-05 14:19:57 +0200

    fixed-route

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("./controller");
 
-router.post("v0/register", register);
-router.post("v0/signin", login);
+router.post("/v0/register", register);
+router.post("/v0/signin", login);
 
 module.exports = router;
