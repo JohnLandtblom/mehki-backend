44fa54589ce09fdc3b9be7e131f82908f6892c8a
Cristian Berglund <Cristian.berglund@hotmail.se>
2022-09-05 12:56:59 +0200

    added-scheme-for-login

---
+++ schemas/userScheme.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userScheme = new mongoose.Schema({
+  firstName: {
+    type: String,
+    trim: true,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: [true, "Last name is required"],
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: [true, "Please enter your email"],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: 5,
+    select: false,
+  },
+  confirmPassword: {
+    type: String,
+    required: ["Confirm your password"],
+    select: false,
+    validate: {
+      validator: function (match) {
+        return match === this.password;
+      },
+      message: "Passwords do not match.",
+    },
+  },
+});
+
+userScheme.methods.comparePassword = async function (
+  enteredPassword,
+  userPassword
+) {
+  return await bcrypt.compare(enteredPassword, userPassword);
+};
+
+module.exports = mongoose.model("User", userScheme);
