6137a49ddf98d1b7fe8521713488500c5f4621bb
Jergie Mascan <jiji.mascan@gmail.com>
2022-09-08 14:41:15 +0200

    login

--- controller.js
+++ controller.js
@@ -25,38 +25,6 @@ exports.register = async (req, res) => {
   });
 };
 
-// exports.register = async (req, res) => {
-//   try {
-//     const newUser = await User.create({
-//       firstName: req.body.firstName,
-//       lastName: req.body.lastName,
-//       email: req.body.email,
-//       password: req.body.password,
-//       confirmPassword: req.body.confirmPassword,
-//     });
-//
-//     res.sendStatus(201).json({
-//       status: "success",
-//       data: {
-//         user: newUser,
-//       },
-//     });
-//
-//     if (!newUser) {
-//       res.sendStatus(401).json({
-//         status: "Failed",
-//         message: "Please enter your email and password!",
-//       });
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     res.sendStatus(401).json({
-//       status: "Failed",
-//       message: "Failed to create account",
-//     });
-//   }
-// };
-
 exports.login = async (req, res) => {
   const { email, password } = await req.body;
 
