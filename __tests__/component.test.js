ce345041f3930c07928ca6904704dab05b37551d
Rimon Hanna <hannarimon1@gmail.com>
2022-09-22 14:23:09 +0200

    added del in comptest

--- __tests__/component.test.js
+++ __tests__/component.test.js
@@ -9,8 +9,10 @@ const app = require("../server");
 
 const db = process.env.MONGODB_URI;
 const PORT = process.env.TEST_PORT || 3002;
+// Required to drop delete user from collection
+// const User = require("../schemas/userSchema");
 
-const testEmail = "lmao@gmail.com";
+const testEmail = "jiji@gmail.se";
 const newUser = {
   firstName: "jiji",
   lastName: "kino",
@@ -26,6 +28,8 @@ describe("Testing API with supertest ", () => {
   });
 
   afterAll(async () => {
+    // Deletes a user after the tests has run successfully (refer to line 13)
+    // await User.deleteOne({ firstName: "patrik" });
     await mongoose.disconnect();
     await server.close();
   });
@@ -76,6 +80,7 @@ describe("Testing API with supertest ", () => {
   it("should delete a user", async () => {
     await request(app)
       .delete("/delete/id")
+      .expect(401)
       .then(function (err, responses) {
         if (err) return err;
       });
